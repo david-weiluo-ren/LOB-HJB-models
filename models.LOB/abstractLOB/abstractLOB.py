2bd33609f4d573d719b2cddfe400610743d1c7af
buzhihuojie <ren.weiluo@gmail.com>
2015-01-27 09:56:23 -0800

    first commit

---
+++ models.LOB/abstractLOB/abstractLOB.py
@@ -0,0 +1,15 @@
+'''
+Created on Jan 27, 2015
+
+@author: weiluo
+'''
+
+class MyClass(object):
+    '''
+    An abstract model as parents for all LOB models, Brownian Motion or Poisson, exp utility or linear with penalty, with
+    price impact or not.
+    '''
+
+
+    def __init__(self, ):
+        pass
